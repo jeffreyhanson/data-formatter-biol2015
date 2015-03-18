bf67106b57290b7675c9f0bfc8525ce3d8cfecdb
paleo13 <jeffrey.hanson@uqconnect.edu.au>
2015-03-19 09:20:39 +1000

    updated correct username for shinyBS repo

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 install.packages("shiny")
-install_github("ebailey/shinyBS")
+install_github("ebailey78/shinyBS")
 install_github("trestletech/shinyTable")
 install.packages("data.table")
 install.packages("plyr")
